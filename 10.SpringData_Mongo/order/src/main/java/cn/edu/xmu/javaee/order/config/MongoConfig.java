0d1eaad390401dcdd7592d5f3d59b60dddcfd9d1
PreciousFlower <162134671+PreciousFlower@users.noreply.github.com>
2024-12-11 12:07:07 +0800

    Update MongoConfig.java

--- 10.SpringData_Mongo/order/src/main/java/cn/edu/xmu/javaee/order/config/MongoConfig.java
+++ 10.SpringData_Mongo/order/src/main/java/cn/edu/xmu/javaee/order/config/MongoConfig.java
@@ -1,36 +1,37 @@
-//School of Informatics Xiamen University, GPL-3.0 license
 package cn.edu.xmu.javaee.order.config;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
+import com.mongodb.ReadPreference;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.hosts}")
+    private String hosts;
+
+    @Value("${spring.data.mongodb.replica-set}")
+    private String replicaSet;
+
     @Value("${spring.data.mongodb.database}")
     private String database;
 
-    @Value("${spring.data.mongodb.host}")
-    private String host;
+    @Value("${spring.data.mongodb.username}")
+    private String username;
 
-    @Value("${spring.data.mongodb.port}")
-    private Integer port;
+    @Value("${spring.data.mongodb.password}")
+    private String password;
 
-    @Bean
-    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
-        return new MongoTransactionManager(dbFactory);
-    }
 
     @Override
     protected String getDatabaseName() {
@@ -39,12 +40,26 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        MongoClient mongoClient = MongoClients.create(
-                MongoClientSettings.builder()
-                        // 集群设置
-                        .applyToClusterSettings(builder ->
-                                builder.hosts(Arrays.asList(new ServerAddress(host, port))))
-                        .build());
-        return mongoClient;
+
+        // 解析主机列表
+        List<ServerAddress> serverAddresses = Arrays.stream(hosts.split(","))
+                .map(host -> {
+                    String[] hostPort = host.split(":");
+                    return new ServerAddress(hostPort[0], Integer.parseInt(hostPort[1]));
+                })
+                .collect(Collectors.toList());
+
+        // 创建认证信息
+        MongoCredential credential = MongoCredential.createCredential(username, database, password.toCharArray());
+
+
+        // 构建客户端设置
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyToClusterSettings(builder -> builder.hosts(serverAddresses)
+                        .requiredReplicaSetName(replicaSet))
+                .credential(credential)
+                .build();
+
+        return MongoClients.create(settings);
     }
 }
